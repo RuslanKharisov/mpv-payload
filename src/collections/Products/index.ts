fa08454b7e1a977c7761d92a426ff1ff6a621b33
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-08-18 19:10:02 +0300

    feat: коллекция products, добавлена поддержка галереи изображений

--- src/collections/Products/index.ts
+++ src/collections/Products/index.ts
@@ -1,5 +1,8 @@
 import { anyone } from '@/access/anyone'
 import { authenticated } from '@/access/authenticated'
+import { CallToAction } from '@/blocks/CallToAction/config'
+import { Content } from '@/blocks/Content/config'
+import { MediaBlock } from '@/blocks/MediaBlock/config'
 import { slugField } from '@/fields/slug'
 import { CollectionConfig } from 'payload'
 
@@ -15,17 +18,98 @@ export const Products: CollectionConfig = {
     singular: 'Продукт',
     plural: 'Продукты',
   },
-  admin: { useAsTitle: 'name', group: 'Tenant-Specific' },
+  admin: {
+    useAsTitle: 'sku',
+    defaultColumns: ['name', 'sku', 'productCategory', 'updatedAt'],
+    group: 'Tenant-Specific',
+  },
   fields: [
-    { name: 'name', label: 'Наименование', type: 'text', required: true },
-    { name: 'sku', type: 'text', required: true, unique: true },
     {
-      name: 'productCategory',
-      type: 'relationship',
-      relationTo: 'product-categories',
-      required: true,
+      type: 'tabs',
+      tabs: [
+        // --- ВКЛАДКА 1: ОСНОВНАЯ ИНФОРМАЦИЯ ---
+        {
+          label: 'Основные',
+          fields: [
+            {
+              name: 'name',
+              label: 'Наименование',
+              type: 'text',
+              required: true,
+              admin: {
+                width: '60%',
+              },
+            },
+            {
+              name: 'sku',
+              label: 'Артикул',
+              type: 'text',
+              required: true,
+              unique: true,
+              admin: {
+                width: '40%',
+              },
+            },
+            {
+              name: 'shortDescription',
+              label: 'Краткое описание',
+              type: 'textarea',
+              admin: {
+                description: 'Появится в карточке товара в общем каталоге.',
+              },
+            },
+            {
+              name: 'productCategory',
+              type: 'relationship',
+              relationTo: 'product-categories',
+              required: true,
+            },
+            {
+              name: 'productImage',
+              label: 'Главное изображение продукта (миниатюра)',
+              type: 'upload',
+              relationTo: 'media',
+              required: true,
+              admin: {
+                description:
+                  'Это изображение будет использоваться в качестве основного на карточке товара и в каталоге.',
+              },
+            },
+            {
+              name: 'images',
+              label: 'Галерея изображений',
+              type: 'array',
+              minRows: 1,
+              labels: {
+                singular: 'Изображение',
+                plural: 'Изображения',
+              },
+              fields: [
+                {
+                  name: 'image',
+                  label: 'Изображение',
+                  type: 'upload',
+                  relationTo: 'media',
+                  required: true,
+                },
+              ],
+            },
+            { name: 'manufacturer', type: 'relationship', relationTo: 'manufacturers' },
+          ],
+        },
+        // --- ВКЛАДКА 2: ТЕХНИЧЕСКИЕ ХАРАКТЕРИСТИКИ ---
+        {
+          label: 'Характеристики',
+          fields: [
+            {
+              type: 'blocks',
+              name: 'layout',
+              blocks: [Content, MediaBlock, CallToAction],
+            },
+          ],
+        },
+      ],
     },
-    { name: 'manufacturer', type: 'relationship', relationTo: 'manufacturers' },
-    ...slugField('name'),
+    ...slugField('sku'),
   ],
 }
