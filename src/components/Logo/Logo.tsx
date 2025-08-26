e35c10a5c9b1c2087b7559b37552a4183b9f77b6
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-08-26 16:46:53 +0300

    Замена логотипа

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,29 +1,12 @@
-import clsx from 'clsx'
+import { LogoNew } from '@/shared/icons/logo-icon'
+import Link from 'next/link'
 import React from 'react'
 
-interface Props {
-  className?: string
-  loading?: 'lazy' | 'eager'
-  priority?: 'auto' | 'high' | 'low'
-}
-
-export const Logo = (props: Props) => {
-  const { loading: loadingFromProps, priority: priorityFromProps, className } = props
-
-  const loading = loadingFromProps || 'lazy'
-  const priority = priorityFromProps || 'low'
-
+export const Logo = () => {
   return (
-    /* eslint-disable @next/next/no-img-element */
-    <img
-      alt="Payload Logo"
-      width={193}
-      height={34}
-      loading={loading}
-      fetchPriority={priority}
-      decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
-    />
+    <Link className="flex items-center space-x-2" href="/">
+      <LogoNew className="h-12 w-12" />
+      <span className="inline-block font-bold">Prom-Stock</span>
+    </Link>
   )
 }
