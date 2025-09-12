4a5e33d32e80025d0d95f8b2fd92f33ff320fd6f
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-09-12 23:32:17 +0300

    страница тарифов

--- src/app/(frontend)/(private)/tariffs/page.tsx
+++ src/app/(frontend)/(private)/tariffs/page.tsx
@@ -1,6 +1,10 @@
 import { cn } from '@/shared/utilities/ui'
 import { CheckIcon } from 'lucide-react'
 
+import { getPayload } from 'payload'
+import configPromise from '@payload-config'
+import Link from 'next/link'
+
 const tiers = [
   {
     name: 'Старт',
@@ -36,10 +40,17 @@ const tiers = [
   },
 ]
 
-function page() {
+async function page() {
+  const payload = await getPayload({ config: configPromise })
+
+  const tariffs = await payload.find({
+    collection: 'tariffs',
+  })
+
+  console.log('tariffs ==> ', tariffs)
   return (
     <div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8 dark:bg-gray-900">
-      <div
+      {/* <div
         aria-hidden="true"
         className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
       >
@@ -50,7 +61,7 @@ function page() {
           }}
           className="mx-auto aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 dark:opacity-20"
         />
-      </div>
+      </div> */}
       <div className="mx-auto max-w-4xl text-center">
         <h2 className="text-base/7 font-semibold text-indigo-600 dark:text-indigo-400">Цены</h2>
         <p className="mt-2 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-6xl dark:text-white">
@@ -62,37 +73,36 @@ function page() {
         лояльности клиентов и стимулирования продаж.
       </p>
       <div className="mx-auto mt-16 grid grid-cols-1  gap-y-6 sm:mt-20 sm:gap-y-0 lg:grid-cols-3 gap-3">
-        {tiers.map((tier, tierIdx) => (
+        {tariffs.docs.map((tariff, idx) => (
           <div
-            key={tier.id}
+            key={tariff.id}
             className={cn(
-              tier.featured
+              idx % 2 !== 0
                 ? 'relative bg-gray-900 shadow-2xl dark:bg-gray-800 dark:shadow-none'
                 : 'bg-white/60 sm:mx-8 lg:mx-0 dark:bg-white/[0.025]',
               'flex flex-col p-8 ring-1 ring-gray-900/10 sm:p-10 dark:ring-white/10',
             )}
           >
             <h3
-              id={tier.id}
               className={cn(
-                tier.featured ? 'text-indigo-400' : 'text-indigo-600 dark:text-indigo-400',
+                idx % 2 !== 0 ? 'text-indigo-400' : 'text-indigo-600 dark:text-indigo-400',
                 'text-base/7 font-semibold',
               )}
             >
-              {tier.name}
+              {tariff.name}
             </h3>
             <p className="mt-4 flex items-baseline gap-x-2">
               <span
                 className={cn(
-                  tier.featured ? 'text-white' : 'text-gray-900 dark:text-white',
+                  idx % 2 !== 0 ? 'text-white' : 'text-gray-900 dark:text-white',
                   'text-5xl font-semibold tracking-tight',
                 )}
               >
-                {tier.priceMonthly}
+                {tariff.price}
               </span>
               <span
                 className={cn(
-                  tier.featured ? 'text-gray-400' : 'text-gray-500 dark:text-gray-400',
+                  idx % 2 !== 0 ? 'text-gray-400' : 'text-gray-500 dark:text-gray-400',
                   'text-base',
                 )}
               >
@@ -101,44 +111,43 @@ function page() {
             </p>
             <p
               className={cn(
-                tier.featured ? 'text-gray-300' : 'text-gray-600 dark:text-gray-300',
+                idx % 2 !== 0 ? 'text-gray-300' : 'text-gray-600 dark:text-gray-300',
                 'mt-6 text-base/7',
               )}
             >
-              {tier.description}
+              {tariff.description}
             </p>
             <ul
               role="list"
               className={cn(
-                tier.featured ? 'text-gray-300' : 'text-gray-600 dark:text-gray-300',
+                idx % 2 !== 0 ? 'text-gray-300' : 'text-gray-600 dark:text-gray-300',
                 'mt-8 space-y-3 text-sm/6 sm:mt-10 flex-grow',
               )}
             >
-              {tier.features.map((feature) => (
-                <li key={feature} className="flex gap-x-3">
+              {tariff.benefits?.map((benefit) => (
+                <li key={benefit.id} className="flex gap-x-3">
                   <CheckIcon
                     aria-hidden="true"
                     className={cn(
-                      tier.featured ? 'text-indigo-400' : 'text-indigo-600 dark:text-indigo-400',
+                      idx % 2 !== 0 ? 'text-indigo-400' : 'text-indigo-600 dark:text-indigo-400',
                       'h-6 w-5 flex-none',
                     )}
                   />
-                  {feature}
+                  {benefit.value}
                 </li>
               ))}
             </ul>
-            <a
-              href={tier.href}
-              aria-describedby={tier.id}
+            <Link
+              href={tariff.name}
               className={cn(
-                tier.featured
+                idx % 2 !== 0
                   ? 'bg-indigo-500 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-indigo-500 dark:shadow-none'
                   : 'text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300 focus-visible:outline-indigo-600 dark:bg-white/10 dark:text-white dark:ring-white/5 dark:hover:bg-white/20 dark:hover:ring-white/5 dark:focus-visible:outline-white/75',
                 'mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10',
               )}
             >
               Get started today
-            </a>
+            </Link>
           </div>
         ))}
       </div>
