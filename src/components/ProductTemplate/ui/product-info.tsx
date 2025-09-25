57b0c25fec8bd7b00cefb56abed58a326b10e40a
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-09-25 20:51:30 +0300

    корректировка стилизации описания товара для мобильного разрешения

--- src/components/ProductTemplate/ui/product-info.tsx
+++ src/components/ProductTemplate/ui/product-info.tsx
@@ -7,12 +7,12 @@ type ProductInfoProps = {
 
 const ProductInfo = ({ product }: ProductInfoProps) => {
   return (
-    <div className="flex flex-col md:gap-y-4 w-full">
-      <Typography tag="h2" variant="inter-bold-32">
+    <div className="flex flex-col gap-y-2 md:gap-y-4 w-full">
+      <Typography tag="h2" variant="inter-md-16">
         {product?.name}
       </Typography>
 
-      <Typography variant="inter-md-16" tag="p">
+      <Typography variant="inter-reg-14" tag="p">
         {product?.shortDescription}
       </Typography>
     </div>
