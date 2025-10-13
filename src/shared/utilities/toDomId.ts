39d2893b9e4ef9073f4847bef8426d5a180ae63d
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-10-13 16:01:16 +0300

    toDomId слагифай value для id и html for атрибутов

--- src/shared/utilities/toDomId.ts
+++ src/shared/utilities/toDomId.ts
@@ -1,8 +1,15 @@
 import slugify from 'slugify'
 
-export const toDomId = (string: string) =>
-  slugify(string, {
+export const toDomId = (string: string) => {
+  const id = slugify(string, {
     lower: true,
     strict: true,
     locale: 'ru',
   })
+
+  if (!id || /^[0-9-]/.test(id)) {
+    return 'id-' + (id || Math.random().toString(36).substring(2, 9))
+  }
+
+  return id
+}
