3a315ac1d3732ed2bd974e322ff8a88c4840f8c4
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-10-26 22:14:54 +0300

    защита от специальных числовых значений zod

--- src/endpoints/import-stocks/index.ts
+++ src/endpoints/import-stocks/index.ts
@@ -7,7 +7,7 @@ const stockRowSchema = z.object({
   sku: z
     .union([z.string(), z.number()]) // Принимаем строку или число
     .transform((val) => String(val).trim()) // Преобразуем в строку и убираем пробелы
-    .refine((str) => str.length > 0, { message: 'SKU не может быть пустым' }),
+    .pipe(z.string().min(1, { message: 'SKU не может быть пустым' })),
   name: z.string(),
   quantity: z.coerce.number({ error: 'Должно быть числом' }),
   price: z.coerce.number().optional(),
