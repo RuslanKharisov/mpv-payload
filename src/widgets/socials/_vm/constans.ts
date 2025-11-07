2a3b422b1badd8f80e424f0fe6b5be3d34ab3f24
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-11-07 18:28:46 +0300

    feat: add new link to socials

--- src/widgets/socials/_vm/constans.ts
+++ src/widgets/socials/_vm/constans.ts
@@ -1,6 +1,7 @@
 import { Telegram } from '@/shared/icons/telegram'
 import { WhatsApp } from '@/shared/icons/whatsapp'
 import { SocialsData } from '../_ui/socials'
+import { PhoneCallIcon } from 'lucide-react'
 
 const WHATSAPP = process.env.NEXT_PUBLIC_WHATSAPP || ''
 const TELEGRAM = process.env.NEXT_PUBLIC_TELEGRAM || ''
@@ -15,5 +16,10 @@ export const socialsData: SocialsData[] = [
     icon: Telegram,
     link: `https://t.me/${TELEGRAM}`,
     label: 'Напишите в Telegram',
+  },
+  {
+    icon: PhoneCallIcon,
+    link: 'tel:+79178696482',
+    label: 'Позвоните нам',
   },
 ]
