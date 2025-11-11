e30a965f680c97abb84f3864eae0bff7032da0e8
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-11-11 22:44:02 +0300

    проверка наличия recaptcha site key

--- src/shared/providers/ReCaptchaProvider/index.tsx
+++ src/shared/providers/ReCaptchaProvider/index.tsx
@@ -4,9 +4,15 @@ import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 import { ReactNode } from 'react'
 
 export const ReCaptchaProvider = ({ children }: { children: ReactNode }) => {
+  const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
+
+  if (!siteKey) {
+    console.error('NEXT_PUBLIC_RECAPTCHA_SITE_KEY is not set')
+    return <>{children}</>
+  }
   return (
     <GoogleReCaptchaProvider
-      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
+      reCaptchaKey={siteKey}
       scriptProps={{
         async: true,
         defer: true,
