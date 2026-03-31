fb168a47009e853681cdfc06e87bf9792d5b0ada
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2026-03-31 16:05:42 +0300

    add russian text for site Name

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -16,7 +16,7 @@ export const Logo = memo(({ className, onClick }: LogoProps) => {
       className={cn('flex justify-center items-center space-x-2', className)}
     >
       <LogoIcon className="h-12 w-12" />
-      <span className="inline-block font-bold">Prom-Stock</span>
+      <span className="inline-block font-bold">Пром-Сток | Prom-Stock</span>
     </Link>
   )
 })
